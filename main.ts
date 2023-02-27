eb52902652429a3f23a72d0ef8f67172af1a65cf
marbellaalfonso <118507739+marbellaalfonso@users.noreply.github.com>
2023-02-27 08:04:34 +0100

    Caritas

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-basic.forever(function on_forever() {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(500)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.pause(500)
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
 })
